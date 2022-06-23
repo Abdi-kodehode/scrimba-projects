152c00a5e2c3cf0010a6919dd5687fa65ffbfb40
Abdi-kodehode <abdi.kodehode@gmail.com>
2022-06-23 13:36:08 +0200

    update

--- Lekser/Many_numbers/index.js
+++ Lekser/Many_numbers/index.js
@@ -6,7 +6,7 @@ btn.addEventListener("click", function() {
     const inputEL = document.getElementById("input-el")
    if (inputEL.value) {
     let sum = [...`${inputEL.value}`].reduce((a, b) => +a + +b)
-    svar.innerHTML += `${sum}, `
+    svar.innerHTML += `${sum}  `
     inputEL.value = ""
    }
    
